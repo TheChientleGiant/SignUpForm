6529479b86fcdf9b79a7a54aebc0869e2b494fa1
TheChientleGiant <thechientlegiant@gmail.com>
2016-02-23 14:35:52 -0800

    Create password if statement on submit click

--- app/src/main/java/com/shomiimages/signupform/LoginActivity.java
+++ app/src/main/java/com/shomiimages/signupform/LoginActivity.java
@@ -5,25 +5,44 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    Button mSubmitButton;
+    EditText mUsernameInput;
+    EditText mPasswordInput;
+    EditText mConfirmInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mSubmitButton = (Button)findViewById(R.id.submit_button);
+        mUsernameInput = (EditText)findViewById(R.id.username_entry);
+        mPasswordInput = (EditText)findViewById(R.id.password_entry);
+        mConfirmInput = (EditText)findViewById(R.id.confirm_entry);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                String username = mUsernameInput.getText().toString();
+                String password = mPasswordInput.getText().toString();
+                String confirmation = mConfirmInput.getText().toString();
+
+                if(password==confirmation){
+                    //match!
+                    Log.d(TAG, "Password Success!");
+                }
+
             }
         });
     }
